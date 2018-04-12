36fc518172d2b85392fc46a9e6e9a6d9648090d9
Olivier DEDOCOTON <jean-jacques-olivier.dedocoton@imt-atlantique.net>
2018-04-12 01:19:07 +0000

    V2 deux methodes implementees

--- challenge1_group7.py
+++ challenge1_group7.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 
 # I am U19886 and I want to work at Google.
-
+ME = 'U19886'
 
 #We load the graph
 G = nx.read_gexf("mediumLinkedin.gexf")
@@ -54,20 +54,11 @@ for i in google_or_nothing_guys :
 #Let's find all the guy from de same college than the Google guys
 #Actually, the college seems (It's just our deduction) to be an important criterea to work at Google
 #Because all the guys working there (and being 'loyal' to the company) are from the same college
-print("############################### COLLEGE GOOGLELIQUE#################################")
+#print("############################### COLLEGE GOOGLELIQUE#################################")
 #print(college)
 '''
-for k,v in college.items():
-    try :
-        if "tongji university" in v:
-            print(k)
-            print(v)
-            print("_____________________________________________")
-    except Exception:
-        continue'''
-
-print("..........................")
-for i in google_guys:
+for i in google_or_nothing_guys:
+    
     try :
         print(str(i))
         print(college[str(i)])
@@ -77,7 +68,18 @@ for i in google_guys:
         print(location[str(i)])
     except Exception :
         print(str(i)+" n'a pas location")
+    
+    try :
+        print(str(i))
+        print(college[str(i)])
+        print(location[str(i)])
+
+    except Exception:
+        continue
+
     print("..........")
+    
+'''
 
 ######################################## Method One ###########################################
 G_cpy = G.copy()
@@ -93,11 +95,11 @@ for guy in google_guys :
     #From here I've  a graph (G_cpy) with one and only node from Google (guy)
     #I'm going to search for the shortest path from him (guy) to me ('U19886')
     try :
-        shortest_paths_to_google.append(nx.dijkstra_path(G_cpy,'U19886',str(guy)))
+        shortest_paths_to_google.append(nx.dijkstra_path(G_cpy,ME,str(guy)))
     except nx.NetworkXNoPath : #If there is not any path between me and current guy
         continue
     G_cpy = G.copy() #We reload the initial graph for the next google guy
-print("Tous les plus courts chemins menant a un employe de Google")
+#print("Tous les plus courts chemins menant a un employe de Google")
 #print(shortest_paths_to_google)
 ############### Displaying shortest paths###############
 '''
@@ -118,3 +120,108 @@ print(employer)
 print(college)
 print(location)
 '''
+############################## Methode Two ###########################
+
+def college_in_common(guy):
+
+    tab = []
+    for k,v in college.items():
+        try :
+            if k == guy :
+                continue
+            else :
+                for i in college[str(guy)] :
+                    for j in v:
+                        if i==j :
+                            tab.append(k)
+        except Exception :
+            continue
+    return tab
+
+def location_in_common(guy):
+    
+    tab = []
+    for k,v in location.items():
+        try :
+            if k == guy :
+                continue
+            else :
+                for i in location[str(guy)] :
+                    for j in v:
+                        if i==j :
+                            tab.append(k)
+        except Exception :
+            continue
+    return tab
+def employer_in_common(guy):
+    
+    tab = []
+    for k,v in employer.items():
+        try :
+            if k == guy :
+                continue
+            else :
+                for i in employer[str(guy)] :
+                    for j in v:
+                        if i==j :
+                            tab.append(k)
+        except Exception :
+            continue
+    return tab
+    
+''' #NEVER EVER RUN THIS CODE: ALMOST INFITE COMPUTATION TIME
+for i in google_guys :
+    for path in nx.all_simple_paths(G,ME,i) :
+        print(path)'''
+
+def h_pertinence(source,target): #Function that will be pass as heuristic parameter in a*
+    point = 0 
+    if source in employer_in_common(target):
+        point=+3
+    if source in location_in_common(target):
+        point=+2
+    if source in college_in_common(target):
+        point=+4
+    if point == 0 : #If the 2 nodes don't have anything in common we look at the neighbors of the tagert
+        for nbr in G.neighbors(target):
+            if target in employer_in_common(nbr):
+                point=+2
+            if target in location_in_common(nbr):
+                point=+1
+            if target in college_in_common(nbr):
+                point=+3
+            if not (point == 0) :
+                break
+    
+    cost = 9-point    #The most of point we have the less we cost and the min of cost is 0
+    return cost
+
+ #Let's fing all the path from ME to all google_or_nothing_guys
+G_cpy = G.copy()
+shortest_star_paths_to_google = []
+for guy in google_or_nothing_guys :
+    #We are going to remove all the nodes from the graph excepted the current "guy"
+    for guy_to_remove in google_or_nothing_guys :
+        if not (guy_to_remove == guy) :
+            try :
+                G_cpy.remove_node(str(guy_to_remove))
+            except nx.NetworkXError :
+                continue
+    #From here I've  a graph (G_cpy) with one and only node from Google (guy)
+    #I'm going to search for the shortest path from him (guy) to me ('U19886')
+    try :
+        shortest_star_paths_to_google.append(nx.astar_path(G_cpy,ME,str(guy),heuristic=h_pertinence))
+    except nx.NetworkXNoPath : #If there is not any path between me and current guy
+        continue
+    G_cpy = G.copy() #We reload the initial graph for the next google guy
+
+#We just have to find the shortest path among all of those retrieved by A*
+best_path = shortest_star_paths_to_google[0]
+
+for path in shortest_star_paths_to_google:
+    if len(path)<len(best_path):
+        best_path = path
+print("''''''''''''''''''''''''''''' LE MEILLEUR CHEMIN POUR AVOIR DU TRAVAIL A GOOGLE ''''''''''''''''''''''''''''''")
+print([d for d in best_path],sep='==>')
+print("Google guys : ")
+print([d for d in google_or_nothing_guys])
