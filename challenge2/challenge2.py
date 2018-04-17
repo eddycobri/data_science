5757b50ebe55c464bd35daf61f7c118414b13996
Olivier DEDOCOTON <jean-jacques-olivier.dedocoton@imt-atlantique.net>
2018-04-17 15:00:20 +0000

    Callenge 2 starting

---
+++ challenge2/challenge2.py
@@ -0,0 +1,32 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from matplotlib import pylab
+import numpy as np
+import pickle
+from collections import Counter
+
+# load the graph
+G = nx.read_gexf("mediumLinkedin.gexf")
+print("Nb of users in our graph: %d" % len(G))
+
+# load the profiles. 3 files for each type of attribute
+# Some nodes in G have no attributes
+# Some nodes may have 1 attribute 'location'
+# Some nodes may have 1 or more 'colleges' or 'employers', so we
+# use dictionaries to store the attributes
+college={}
+location={}
+employer={}
+# The dictionaries are loaded as dictionaries from the disk (see pickle in Python doc)
+with open('mediumCollege_60percent_of_empty_profile.pickle', 'rb') as handle:
+    college = pickle.load(handle)
+with open('mediumLocation_60percent_of_empty_profile.pickle', 'rb') as handle:
+    location = pickle.load(handle)
+with open('mediumEmployer_60percent_of_empty_profile.pickle', 'rb') as handle:
+    employer = pickle.load(handle)
+
+# Let's begin with homophily
+for k,v in college :
+    print(k)
+    for i in v :
+        print(i)
