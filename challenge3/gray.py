82aa4d866be0b7a26024340f244eb33540c3b383
eddycobri <eddycobri@gmail.com>
2018-05-28 15:04:21 +0200

    the gray

--- challenge3/gray.py
+++ challenge3/gray.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from skimage.graph import route_through_array
 
-input_filename="/home/rudy/Desktop/population-density-map.bmp"
+input_filename="C:/population-density-map.bmp"
 
 the_map = Image.open(input_filename)
 width=the_map.size[0]
@@ -20,26 +20,55 @@ brest=[2108,4426]  #coor de brest
 rize=[1306,669]          # coor de rize
 
 
-the_map_gray.show()
+#the_map_gray.show()
 colors = the_map_gray.getcolors(width*heigth)
 print('Nb of different colors: %d' % len(colors))
 
 
+#la fonction si_dessous permet de reduire une matrice...
+
+def matrix_transformer (the_matrix, n):
+    new_matrix=[]
+    (x,y)=np.shape(the_matrix)
+    z,t=0,0
+    while z<x:
+        ligne=[]
+        while t<y:
+            print(the_matrix[z:z+n,t:t+n])
+            moy=np.mean(the_matrix[z:z+n,t:t+n])
+            print("la moyenne est :"+str(moy)+" pour "+ str(t)+"\n")
+            ligne.append(moy)
+            t+=n
+        new_matrix.append(np.array(ligne))
+        print("000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000  ")
+        t=0
+        z+=n
+    return np.array(new_matrix)            
+    
+
 
 # from gray colors to density
 density = gray_matrix/255.0
-
+"""
 #the shortest path used by the zombie for Rize to brest is the path consisted of city with higher density , so
 the_path=route_through_array(density, rize, brest)
 
 pixelArray = the_map.load() # pixel array
 for i in the_path[0]:
-	pixelArray[i[1],i[0]] = (255,0,0,255) # change pixel color
+	pixelArray[i[1],i[0]] = (0.1, 0.2, 0.5, 0.3) # change pixel color
 the_map.show()
 
 
+"""
 
 print(density)
+reduced_density=matrix_transformer(density, 10)
+print(reduced_density)
+a=np.size(density)
+print(a)
+b=np.shape(density)
+print(b)
+
 """
 plt.imshow(density)
 plt.plot(4426,2108,'r*')
@@ -57,4 +86,5 @@ gray = rgb2gray(img)
 plt.imshow(gray, cmap = plt.get_cmap('gray'))
 plt.show()
 """
+
 
